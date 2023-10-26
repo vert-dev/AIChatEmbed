0e44dc0863f8393c167735bd4e7401f397b1e44a
Clarence Cannon <clarence@cannon.cloud>
2023-10-26 11:03:30 -0700

    updated colors

--- src/components/bubbles/BotBubble.tsx
+++ src/components/bubbles/BotBubble.tsx
@@ -1,28 +1,28 @@
-import { Show, onMount } from 'solid-js'
-import { Avatar } from '../avatars/Avatar'
-import { Marked } from '@ts-stack/markdown'
+import { Show, onMount } from 'solid-js';
+import { Avatar } from '../avatars/Avatar';
+import { Marked } from '@ts-stack/markdown';
 
 type Props = {
-  message: string
-  showAvatar?: boolean
-  avatarSrc?: string
-  backgroundColor?: string
-  textColor?: string
-}
+  message: string;
+  showAvatar?: boolean;
+  avatarSrc?: string;
+  backgroundColor?: string;
+  textColor?: string;
+};
 
-const defaultBackgroundColor = '#f7f8ff'
-const defaultTextColor = '#303235'
+const defaultBackgroundColor = '#f7f8ff';
+const defaultTextColor = '#303235';
 
-Marked.setOptions({ isNoP: true })
+Marked.setOptions({ isNoP: true });
 
 export const BotBubble = (props: Props) => {
-  let botMessageEl: HTMLDivElement | undefined
+  let botMessageEl: HTMLDivElement | undefined;
 
   onMount(() => {
     if (botMessageEl) {
-      botMessageEl.innerHTML = Marked.parse(props.message)
+      botMessageEl.innerHTML = Marked.parse(props.message);
     }
-  })
+  });
 
   return (
     <div
@@ -36,8 +36,12 @@ export const BotBubble = (props: Props) => {
         ref={botMessageEl}
         class="px-4 py-2 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble"
         data-testid="host-bubble"
-        style={{ "background-color": props.backgroundColor ?? defaultBackgroundColor, color: props.textColor ?? defaultTextColor, 'border-radius': '6px' }}
+        style={{
+          'background-color': props.backgroundColor ?? defaultBackgroundColor,
+          color: props.textColor ?? defaultTextColor,
+          'border-radius': '6px',
+        }}
       />
     </div>
-  )
-}
+  );
+};
